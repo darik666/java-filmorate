07b0fe617907df6cd81b4be1202dc12bd0da6d8d
darik666 <tattsar666@yandex.ru>
2023-04-12 14:51:20 -0700

    TZ11

--- src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -41,6 +42,20 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleRuntimeEx(final RuntimeException e) {
+        log.warn(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        log.warn(e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleNotCatchedExceptions(final Exception e) {
         log.warn(e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
