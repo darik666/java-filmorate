5aecfdf1fe6caffe0893a41a18336206ff5a1d4a
darik666 <115643706+darik666@users.noreply.github.com>
2023-04-11 19:38:57 -0700

    Update Film.java

--- src/main/java/ru/yandex/practicum/model/Film.java
+++ src/main/java/ru/yandex/practicum/model/Film.java
@@ -28,6 +28,4 @@ public class Film {
     private Set<Integer> likes = new HashSet<>();
     private Set<Genre> genres = new TreeSet<>();
     private MPA mpa = new MPA();
-    private Set<Genre> genres = new HashSet<>();
-    private MPA mpa;
 }
